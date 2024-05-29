7222aceeb06607e0ac41361d841e36a982be314a
Harsh Wardhan <wardhan071@gmail.com>
2024-05-29 12:51:07 +0530

    second commit

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,99 +1,69 @@
-import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { validateEmail } from '../validation/emailValidator';
-import { validatePassword } from '../validation/passwordValidator';
-import jwt, { JwtPayload } from 'jsonwebtoken';
-const bcrypt = require('bcrypt');
-import dotenv from 'dotenv';
-import { User } from '../entities/User';
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { validateEmail } from "../validation/emailValidator";
+import { validatePassword } from "../validation/passwordValidator";
+import jwt, { JwtPayload } from "jsonwebtoken";
+const bcrypt = require("bcrypt");
+import dotenv from "dotenv";
+import { User } from "../entities/User";
+import { request } from "http";
 
 export class UserController {
-  /* static getAll = async (req: Request, res: Response) => {
+  static getAll = async (req: Request, res: Response) => {
     const userRepository = getRepository(User);
     const users = await userRepository.find();
     res.send(users);
-  }; */
-
-
-  // http://localhost:3000/api/users?page=2&pageSize=10
-
-  static getAll = async (req: Request, res: Response) => {
-    const userRepository = getRepository(User);
-    const page = parseInt(req.query.page as string) || 1;
-    const pageSize = parseInt(req.query.pageSize as string) || 10;
-    try {
-      const skip = (page - 1) * pageSize;
-      const [users, total] = await userRepository.findAndCount({
-        skip: skip,
-        take: pageSize,
-        select: ["id", "name", "email", "contact"]
-      });
-      res.send({
-        page,
-        pageSize,
-        total,
-        totalPages: Math.ceil(total / pageSize),
-        users,
-      });
-    } catch (error) {
-      console.error('Error fetching users:', error);
-      res.status(500).send({ message: 'Error fetching users' });
-    }
   };
 
-  // static login = async (req: Request, res: Response) => {
-  //   console.log('log in')
-    
-  //   const userRepository = getRepository(User);
-  //   const user = await userRepository.findOne({
-  //     where: { email: req.body.email , password: req.body.password},       
-  //   });
-  //   console.log(user);
+  static listing = async (req: Request, res: Response) => {
+    try {
+      const { filter, page, pageSize, order, orderBy } = req.body;
+      const userRepository = getRepository(User);
 
-   
-  //   const [userResult] = await userRepository.find({
-  //     select: ["id", "name", "email", "contact"],
-  //   })
-  //   console.log(userResult) 
-  //   if (!user) {
-  //     return res.status(401).send({ message: 'Invalid email or password' });
-  //   }
-    
-  //     const token = jwt.sign({email: userResult.email, id: userResult.id}, process.env.JWT_SECRET as string , { expiresIn: '1h' } );
-  //     console.log(token)
-  //     res.send({...userResult, token });
-  // }
+      let query = userRepository.createQueryBuilder("user");
 
+        switch (filter.search) {
+          case "name": 
+            query = query.where("user.name LIKE :name", {
+              name: `%${filter.search}%`,
+            });
+            break;
+            
+          case "contact":
+            query = query.where("user.contact = :contact", {
+              contact : `${filter.search}`
+            });
+            break;
 
-  static login = async (req: Request, res: Response) => {
-    console.log('log in');
-    const { email, password } = req.body;
-    const userRepository = getRepository(User);
-    const user = await userRepository.findOne({ where: { email } });
-    if (!user) {
-        return res.status(401).send({ message: 'Invalid email or password' });
-    }
-    // Compare passwords
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
-        return res.status(401).send({ message: 'Invalid email or password' });
-    }
-    // Generate token
-    const token = jwt.sign(
-        { email: user.email, id: user.id },
-        process.env.JWT_SECRET as string,
-        { expiresIn: '1h' }
-    );
-    console.log(token);
-    // Send response with minimal user info and token
-    res.send({
+          case "email":
+            query = query.where("user.email = :email", {
+              email : `${filter.search}`
+            });
+            break;
+        }
+
+      if (order && orderBy) {
+        query = query.orderBy(`user.${orderBy}`, order);
+      }
+
+      if (page && pageSize) {
+        query = query.skip((page - 1) * pageSize).take(pageSize);
+      }
+
+      const users = await query.getMany();
+
+      const formattedUsers = users.map((user) => ({
         id: user.id,
         name: user.name,
         email: user.email,
         contact: user.contact,
-        token
-    });
-}
+      }));
+      res.send(formattedUsers);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+      res.status(500).send({ message: "Error fetching users" });
+    }
+  };
 
   static create = async (req: Request, res: Response) => {
     const userRepository = getRepository(User);
@@ -101,44 +71,55 @@ export class UserController {
     if (!email || !password) {
       res.status(400).json({ error: "Email and password are required." });
       return;
-  }
-  if (!validateEmail(email)) {
+    }
+    if (!validateEmail(email)) {
       res.status(400).json({ error: "Invalid email address." });
       return;
-  }
-  if (!validatePassword(password)) {
-      res.status(400).json({ error: "Password must be at least 8 characters long." });
+    }
+    if (!validatePassword(password)) {
+      res
+        .status(400)
+        .json({ error: "Password must be at least 8 characters long." });
       return;
-  }
-    const user = userRepository.create({ name, email, contact, password });   
+    }
+    const user = userRepository.create({ name, email, contact, password });
     const result = await userRepository.save(user);
-    res.send({ id: result.id, 
-      name: result.name, 
-      email: result.email, 
+    res.send({
+      id: result.id,
+      name: result.name,
+      email: result.email,
       contact: result.contact,
-    })
-
+    });
   };
 
-  static getUsersByName = async (req: Request, res: Response) => {
+  static login = async (req: Request, res: Response) => {
+    console.log("log in");
+    const { email, password } = req.body;
     const userRepository = getRepository(User);
-  
-    try {
-      const name = req.params.name;
-      const users = await userRepository.find({
-        where: { name: name },
-        select: ["id", "name", "email", "contact"],
-      });
-  
-      if (users.length > 0) {
-        res.send(users);
-      } else {
-        res.status(404).send({ message: 'No users found with that name' });
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).send({ message: 'Internal Server Error' });
+    const user = await userRepository.findOne({ where: { email } });
+    if (!user) {
+      return res.status(401).send({ message: "Invalid email or password" });
     }
+    // Compare passwords
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).send({ message: "Invalid email or password" });
+    }
+    // Generate token
+    const token = jwt.sign(
+      { email: user.email, id: user.id },
+      process.env.JWT_SECRET as string,
+      { expiresIn: "1h" }
+    );
+    console.log(token);
+    // Send response with minimal user info and token
+    res.send({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      contact: user.contact,
+      token,
+    });
   };
 
   static update = async (req: Request, res: Response) => {
@@ -148,13 +129,13 @@ export class UserController {
     });
     const [userResult] = await userRepository.find({
       select: ["id", "name", "email", "contact"],
-    })
+    });
     if (user) {
       userRepository.merge(user, req.body);
       const result = await userRepository.save(user);
       res.send(userResult);
     } else {
-      res.status(404).send({ message: 'User not found' });
+      res.status(404).send({ message: "User not found" });
     }
   };
 
@@ -165,9 +146,76 @@ export class UserController {
     });
     if (user) {
       await userRepository.remove(user);
-      res.send({ message: 'User deleted successfully' });
+      res.send({ message: "User deleted successfully" });
     } else {
-      res.status(404).send({ message: 'User not found' });
+      res.status(404).send({ message: "User not found" });
     }
   };
 }
+// static getUsersByName = async (req: Request, res: Response) => {
+//   const userRepository = getRepository(User);
+
+//   try {
+//     const name = req.params.name;
+//     const users = await userRepository.find({
+//       where: { name: name },
+//       select: ["id", "name", "email", "contact"],
+//     });
+
+//     if (users.length > 0) {
+//       res.send(users);
+//     } else {
+//       res.status(404).send({ message: 'No users found with that name' });
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send({ message: 'Internal Server Error' });
+//   }
+// };
+// http://localhost:3000/api/users?page=2&pageSize=10
+
+// static getAll = async (req: Request, res: Response) => {
+//   const userRepository = getRepository(User);
+//   const page = parseInt(req.query.page as string) || 1;
+//   const pageSize = parseInt(req.query.pageSize as string) || 10;
+//   try {
+//     const skip = (page - 1) * pageSize;
+//     const [users, total] = await userRepository.findAndCount({
+//       skip: skip,
+//       take: pageSize,
+//       select: ["id", "name", "email", "contact"]
+//     });
+//     res.send({
+//       page,
+//       pageSize,
+//       total,
+//       totalPages: Math.ceil(total / pageSize),
+//       users,
+//     });
+//   } catch (error) {
+//     console.error('Error fetching users:', error);
+//     res.status(500).send({ message: 'Error fetching users' });
+//   }
+// };
+
+// static login = async (req: Request, res: Response) => {
+//   console.log('log in')
+
+//   const userRepository = getRepository(User);
+//   const user = await userRepository.findOne({
+//     where: { email: req.body.email , password: req.body.password},
+//   });
+//   console.log(user);
+
+//   const [userResult] = await userRepository.find({
+//     select: ["id", "name", "email", "contact"],
+//   })
+//   console.log(userResult)
+//   if (!user) {
+//     return res.status(401).send({ message: 'Invalid email or password' });
+//   }
+
+//     const token = jwt.sign({email: userResult.email, id: userResult.id}, process.env.JWT_SECRET as string , { expiresIn: '1h' } );
+//     console.log(token)
+//     res.send({...userResult, token });
+// }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,9 @@ import { UserController } from '../controllers/UserController';
 
 const routes = Router();
 
+routes.post('/users/listing', UserController.listing);
 routes.get('/users', UserController.getAll);
 routes.post('/users', UserController.create);
-routes.get('/users/:name', UserController.getUsersByName);
 routes.put('/users/:id', UserController.update);
 routes.delete('/users/:id', UserController.delete);
 routes.post('/users/login', UserController.login);
